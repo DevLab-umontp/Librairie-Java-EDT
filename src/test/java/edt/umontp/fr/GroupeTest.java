845be6201d627a5b6c8be7805bcef9c884e9e8dd
emerick-biron <emerick.biron@gmail.com>
2021-01-18 17:16:31 +0100

    :hammer: maj tests Groupe

--- src/test/java/edt/umontp/fr/GroupeTest.java
+++ src/test/java/edt/umontp/fr/GroupeTest.java
@@ -2,10 +2,13 @@ package edt.umontp.fr;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GroupeTest {
 
     @Test
     void test_getIntitule() {
+        assertEquals("A1",Groupe.A1.getIntitule());
     }
 
     @Test

f8745bf153367c8b4ffb4ddb64bbe0708e3f4809
emerick-biron <emerick.biron@gmail.com>
2021-01-18 17:10:05 +0100

    :hammer: test Groupe

--- src/test/java/edt/umontp/fr/GroupeTest.java
+++ src/test/java/edt/umontp/fr/GroupeTest.java
@@ -1,5 +1,22 @@
 package edt.umontp.fr;
 
+import org.junit.jupiter.api.Test;
+
 public class GroupeTest {
-    
+
+    @Test
+    void Test_getIntitule() {
+    }
+
+    @Test
+    void contient() {
+    }
+
+    @Test
+    void values() {
+    }
+
+    @Test
+    void valueOf() {
+    }
 }

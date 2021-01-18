ccf552ae602b7d4a5f558f4a6e21c448769ee8eb
emerick-biron <emerick.biron@gmail.com>
2021-01-18 10:57:55 +0100

    First commit

---
+++ src/main/java/edt/umontp/fr/EmploiDuTemps.java
@@ -0,0 +1,12 @@
+package edt.umontp.fr;
+
+import java.io.File;
+
+public class EmploiDuTemps {
+    private final String LIEN_ICAL = "";
+    private final EmploiDuTemps singleton = null;
+
+    public File getIcs (String LIEN_ICAL){
+
+    }
+}
---
+++ src/main/java/edt/umontp/fr/Test.java
@@ -0,0 +1,7 @@
+package edt.umontp.fr;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}

b6fe1ac1da604bdb57f88e9ed5acd6a105f7e121
emerick-biron <emerick.biron@gmail.com>
2021-01-18 16:26:46 +0100

    :hammer: maj getProfFromDesc

--- src/main/java/edt/umontp/fr/Test.java
+++ src/main/java/edt/umontp/fr/Test.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(Cours.getProfFromDesc("\n\nA1-Semestre-1\nGOU A_ICH   ABDELKADER\nLAGUILLAUMIE   FABIEN\nPOUPET   VICTOR\nROSENFELD   MATTHIEU\nA valider\n(Exporté le:18/01/2 021 10:51)\n")));
+        System.out.println(Arrays.toString(Cours.getProfFromDesc("\n\nA1-Semestre-1\nGOU A_ICH   " +
+                "ABDELKADER\nLAGUILLAUMIE   FABIEN\nPOUPET   VICTOR\nROSENFELD   MATTHIEU\nA valider\n(Exporté " +
+                "le:18/01/2 021 10:51)\n")));
     }
 }
--- src/test/java/edt/umontp/fr/CoursTest.java
+++ src/test/java/edt/umontp/fr/CoursTest.java
@@ -2,24 +2,31 @@ package edt.umontp.fr;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CoursTest {
 
     @Test
     void Test_getGroupeFromDesc_A1Seul_A1() {
-        String desc = "\n\nA1\nGOUAICH   ABDELKADER\nLAGUILLAUMIE   FABIEN\nPOUPET VICTOR\nROSENFELD MATTHIEU\nA valider\n(Exporté le:18 / 01 / 2 021 10:51)\n";
+        String desc = "\n\nA1\nGOUAICH   ABDELKADER\nLAGUILLAUMIE   FABIEN\nPOUPET VICTOR\nROSENFELD MATTHIEU\nA " +
+                "valider\n(Exporté le:18 / 01 / 2 021 10:51)\n";
         assertEquals(Groupe.A1, Cours.getGroupeFromDesc(desc));
     }
 
     @Test
     void Test_getGroupeFromDesc_A1NonSeul_A1() {
-        String desc = "\n\nA1-Semestre-1\nGOUAICH   ABDELKADER\nLAGUILLAUMIE   FABIEN\nPOUPET   VICTOR\nROSENFELD   MATTHIEU\nA valider\n(Exporté le:18/01/2 021 10:51)\n";
+        String desc = "\n\nA1-Semestre-1\nGOUAICH   ABDELKADER\nLAGUILLAUMIE   FABIEN\nPOUPET   VICTOR\nROSENFELD   " +
+                "MATTHIEU\nA valider\n(Exporté le:18/01/2 021 10:51)\n";
         assertEquals(Groupe.A1, Cours.getGroupeFromDesc(desc));
     }
 
     @Test
     void Test_getProfFromDdesc_Desc_NomsDesProfs() {
-        assertTrue(true);
+        String desc = "\n\nA2-Semestre-3\nBELMECHERI   NASSIM\nHAETTEL   THOMAS\nLA   XUAN HOANG\nCHIROUZE   ANNE\nA "
+                + "valider\n(Exporté le:18/01/2021 10:51)\n";
+        String[] expected = {"BELMECHERI   NASSIM", "HAETTEL   THOMAS", "LA   XUAN HOANG", "CHIROUZE   ANNE"};
+        assertEquals(Arrays.toString(expected), Arrays.toString(Cours.getProfFromDesc(desc)));
     }
 }

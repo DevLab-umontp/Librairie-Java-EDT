06a43122fcb8ee5a692f782356e5b7f909e40355
MathieuSoysal <Mathieu.soysal@outlook.fr>
2021-01-19 20:04:39 +0100

    📝 Mise à jour JavaDoc Planifiable

--- src/main/java/edt/umontp/fr/Planifiable.java
+++ src/main/java/edt/umontp/fr/Planifiable.java
@@ -3,7 +3,8 @@ package edt.umontp.fr;
 import java.time.LocalDate;
 
 /**
- * Planifiable est une interface proposant des méthodes pour gérer un emploi du temps
+ * Planifiable est une interface proposant des méthodes pour gérer un emploi du
+ * temps
  *
  * @author emerick-biron
  * @author MathieuSoysal
@@ -11,30 +12,38 @@ import java.time.LocalDate;
  */
 interface Planifiable {
     /**
-     * Permet d'obtenir le planning correspondant a une date
+     * Permet d'obtenir le planning correspondant à une date
      *
      * @param date date pour laquelle on veut obtenir le planning
-     * @return planning corresspndant
+     * @return {@code Planning} correspondant
      * @since 1.0
+     * 
+     * @see Planning
      */
     public Planning getPlanningOf(LocalDate date);
 
     /**
-     * Permet d'obtenir le planning correspondant a une date et un groupe
+     * Permet d'obtenir le planning correspondant à une date et un groupe
      *
      * @param date   date dont on veut obtenir le planning
      * @param groupe groupe dont on veut obtenir le planning
      * @return planning correspondant
      * @since 1.0
+     * 
+     * @see Groupe
+     * @see Planning
      */
     public Planning getPlanningOf(LocalDate date, Groupe groupe);
 
     /**
-     * Permet d'obtenir le planning correspondant a un groupe
+     * Permet d'obtenir le planning correspondant à un groupe
      *
      * @param groupe groupe dont on veut obtenir le planning
      * @return planning correspondant
      * @since 1.0
+     * 
+     * @see Groupe
+     * @see Planning
      */
     public Planning getPlanningOf(Groupe groupe);
 

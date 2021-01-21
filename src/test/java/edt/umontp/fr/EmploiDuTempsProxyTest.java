5b64337b9912860c5f9441228fc7e7854ed2d78c
MathieuSoysal <Mathieu.soysal@outlook.fr>
2021-01-21 12:55:04 +0100

    ✅ Mise à jour tests : EmploiDuTempsProxyTest

--- src/test/java/edt/umontp/fr/EmploiDuTempsProxyTest.java
+++ src/test/java/edt/umontp/fr/EmploiDuTempsProxyTest.java
@@ -44,7 +44,17 @@ class EmploiDuTempsProxyTest {
         emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(7));
         emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(8));
         emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(9));
-        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(10));
+        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(10));        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(1));
+        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(11));        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(1));
+        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(12));
+        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(13));
+        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(14));
+        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(15));
+        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(16));
+        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(17));
+        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(18));
+        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(19));
+
         return System.currentTimeMillis() - startTime;
     }
 
@@ -60,6 +70,10 @@ class EmploiDuTempsProxyTest {
 
     private long rapiditeGetPlanningOfGroupe() {
         long startTime = System.currentTimeMillis();
+        emploiDuTempsProxy.getPlanningOf(Groupe.S1);
+        emploiDuTempsProxy.getPlanningOf(Groupe.S2);
+        emploiDuTempsProxy.getPlanningOf(Groupe.S3);
+        emploiDuTempsProxy.getPlanningOf(Groupe.S4);
         emploiDuTempsProxy.getPlanningOf(Groupe.A1);
         emploiDuTempsProxy.getPlanningOf(Groupe.Q1);
         emploiDuTempsProxy.getPlanningOf(Groupe.Q2);
@@ -95,6 +109,15 @@ class EmploiDuTempsProxyTest {
         emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(8), Groupe.G3);
         emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(9), Groupe.G4);
         emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(10), Groupe.A2);
+        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(11), Groupe.A1);
+        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(12), Groupe.Q1);
+        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(13), Groupe.Q2);
+        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(14), Groupe.Q3);
+        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(15), Groupe.Q4);
+        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(16), Groupe.G1);
+        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(17), Groupe.G2);
+        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(18), Groupe.G3);
+        emploiDuTempsProxy.getPlanningOf(LocalDate.now().plusDays(19), Groupe.G4);
         return System.currentTimeMillis() - startTime;
     }
 

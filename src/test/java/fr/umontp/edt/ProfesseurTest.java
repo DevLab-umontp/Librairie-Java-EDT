e8450ac7d2ac7c1a06ae1bcf3bc0d51200383c4b
MathieuSoysal <Mathieu.soysal@outlook.fr>
2021-01-26 23:30:40 +0100

    ✅ Mise à jour tests : ProfesseurTest

--- src/test/java/fr/umontp/edt/ProfesseurTest.java
+++ src/test/java/fr/umontp/edt/ProfesseurTest.java
@@ -15,21 +15,21 @@ class ProfesseurTest {
 
     @Test
     void testConstructeur_UnStrings_prenomNomCombine() {
-        Professeur professeur = new Professeur("Jean   Dupuis");
+        Professeur professeur = new Professeur("Dupuis   Jean");
         assertEquals("Dupuis", professeur.getNom());
         assertEquals("Jean", professeur.getPrenom());
     }
 
     @Test
     void testEquals_deuxProfesseursIdentiqueAvecConstructeurDifferent() {
-        Professeur professeur1 = new Professeur("Jean   Dupuis");
+        Professeur professeur1 = new Professeur("Dupuis   Jean");
         Professeur professeur2 = new Professeur("Dupuis", "Jean");
         assertEquals(professeur1, professeur2);
     }
 
     @Test
     void testEquals_deuxProfesseursIdentiqueAvecConstructeurDifferent_avecDifferenceMajuscule() {
-        Professeur professeur1 = new Professeur("JeAn   DUpuis");
+        Professeur professeur1 = new Professeur("DUpuis   JeAN");
         Professeur professeur2 = new Professeur("DupUis", "JEan");
         assertEquals(professeur1, professeur2);
     }

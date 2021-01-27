a92409104c8d4061a27a64529db5a66b73ce2ffe
MathieuSoysal <Mathieu.soysal@outlook.fr>
2021-01-27 22:49:47 +0100

    ✅ Ajout test validé

--- src/test/java/fr/umontp/edt/PlanningFiltreurTest.java
+++ src/test/java/fr/umontp/edt/PlanningFiltreurTest.java
@@ -1,5 +1,6 @@
 package fr.umontp.edt;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
@@ -14,4 +15,13 @@ class PlanningFiltreurTest {
 
         assertEquals(LocalDate.now(), planningFiltreur.getDates()[0]);
     }
+
+    @Test
+    void test_PlanningFiltreur_par_plusieursDates() {
+        PlanningFiltreur planningFiltreur = PlanningFiltreur.filtrer().par(LocalDate.now(), LocalDate.now().plusDays(1),
+                LocalDate.now().plusDays(2));
+        LocalDate[] excepted = { LocalDate.now(), LocalDate.now().plusDays(1), LocalDate.now().plusDays(2) };
+        assertArrayEquals(excepted, planningFiltreur.getDates());
+    }
+
 }
